71325dd855028b297460db0ad64886057ad28ec8
aukro1408 <142673562+aukro1408@users.noreply.github.com>
2025-11-10 23:36:03 +0200

    Update and rename 4kino6.js to hren.js

---
+++ hren.js
@@ -0,0 +1,23 @@
+// ==LampaPlugin==
+// name: Хрень
+// type: menu
+// ==/LampaPlugin==
+
+Lampa.Plugin.add('hren', {
+    title: 'Хрень',
+    icon: 'data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg>',
+    in_menu: true,
+    start: function () {
+        let html = Lampa.Menu.cards();
+        html.empty();
+
+        Lampa.Activity.push({
+            name: 'hren',
+            onBack: function () {},
+            onExit: function () {},
+            render: function () {
+                return html.render();
+            }
+        });
+    }
+});
