3b829df6c9dd2e799e7b1d30727daa9a55028a63
aukro1408 <142673562+aukro1408@users.noreply.github.com>
2026-05-12 15:07:33 +0300

    Update kbteam.js

--- kbteam.js
+++ kbteam.js
@@ -1,35 +1,12 @@
 (function () {
     'use strict';
 
-    function startPlugin() {
-        console.log('KBTeam plugin started');
+    console.log('KBTeam plugin loaded');
 
-        if (!window.Lampa) {
-            console.log('Lampa not found');
-            return;
-        }
-
-        Lampa.Noty.show('KBTeam plugin loaded');
-
-        var source = {
-            title: 'KBTeam',
-            search: function (params, oncomplite) {
-                console.log('KBTeam search:', params);
-
-                oncomplite({
-                    results: []
-                });
-            }
-        };
-
-        Lampa.Api.sources.tmdb = source;
-    }
-
-    if (window.appready) {
-        startPlugin();
+    if (window.Lampa) {
+        Lampa.Noty.show('KBTeam OK');
     } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type == 'ready') startPlugin();
-        });
+        alert('KBTeam OK');
     }
+
 })();
