f4f617a3a9180a30a9403485dc13fccd8c8a8fa0
aukro1408 <142673562+aukro1408@users.noreply.github.com>
2026-05-12 15:21:38 +0300

    Update kbteam.js

--- kbteam.js
+++ kbteam.js
@@ -1,82 +1,130 @@
 (function () {
     'use strict';
 
+    function createButton(movie) {
+
+        var button = $('<div class="simple-button selector kbteam-button">KBTeam</div>');
+
+        button.on('hover:enter', function () {
+
+            Lampa.Noty.show('KBTeam loading...');
+
+            fetch(
+                'http://kb-team.club/msx/kinozal/videocdn.php?act=watch&vid='
+                + movie.kinopoisk_id
+            )
+            .then(function (r) {
+                return r.json();
+            })
+            .then(function (json) {
+
+                console.log('KBTEAM WATCH:', json);
+
+                if (!json || !json.items || !json.items.length) {
+                    Lampa.Noty.show('No stream found');
+                    return;
+                }
+
+                var item = null;
+
+                json.items.forEach(function (x) {
+
+                    if (
+                        !item &&
+                        x.action &&
+                        typeof x.action === 'string' &&
+                        x.action.indexOf('video:') === 0
+                    ) {
+                        item = x;
+                    }
+
+                });
+
+                if (!item) {
+                    Lampa.Noty.show('No playable video');
+                    return;
+                }
+
+                var url = item.action.replace('video:', '');
+
+                console.log('PLAY URL:', url);
+
+                Lampa.Player.play({
+                    title: movie.title || movie.name || 'KBTeam',
+                    url: url
+                });
+
+            })
+            .catch(function (err) {
+
+                console.log(err);
+
+                Lampa.Noty.show('KBTeam error');
+
+            });
+
+        });
+
+        return button;
+    }
+
+    function injectButton(movie) {
+
+        setTimeout(function () {
+
+            var panel = $(
+                '.full-start-new__buttons, ' +
+                '.full-start__buttons, ' +
+                '.full-buttons, ' +
+                '.buttons'
+            );
+
+            console.log('KBTEAM PANEL:', panel);
+
+            if (!panel.length) {
+                console.log('KBTEAM: buttons panel not found');
+                return;
+            }
+
+            if (panel.find('.kbteam-button').length) return;
+
+            panel.append(createButton(movie));
+
+            console.log('KBTEAM: button added');
+
+        }, 1500);
+
+    }
+
     function startPlugin() {
+
+        console.log('KBTeam plugin started');
 
         Lampa.Noty.show('KBTeam online ready');
 
         Lampa.Listener.follow('full', function (e) {
 
-            if (!e || !e.data || !e.data.movie) return;
+            try {
 
-            var movie = e.data.movie;
+                if (!e || !e.data || !e.data.movie) return;
 
-            if (!movie.kinopoisk_id) return;
+                var movie = e.data.movie;
 
-            console.log('KBTEAM MOVIE:', movie);
+                console.log('KBTEAM MOVIE:', movie);
 
-            var button = $('<div class="simple-button selector">KBTeam</div>');
-
-            button.on('hover:enter', function () {
-
-                Lampa.Noty.show('Loading stream...');
-
-                fetch(
-                    'http://kb-team.club/msx/kinozal/videocdn.php?act=watch&vid='
-                    + movie.kinopoisk_id
-                )
-                .then(function (r) {
-                    return r.json();
-                })
-                .then(function (json) {
-
-                    console.log(json);
-
-                    if (!json || !json.items || !json.items.length) {
-                        Lampa.Noty.show('No stream found');
-                        return;
-                    }
-
-                    var item = json.items.find(function (x) {
-                        return x.action && x.action.indexOf('video:') === 0;
-                    });
-
-                    if (!item) {
-                        Lampa.Noty.show('No playable video');
-                        return;
-                    }
-
-                    var url = item.action.replace('video:', '');
-
-                    Lampa.Player.play({
-                        title: movie.title,
-                        url: url
-                    });
-
-                })
-                .catch(function (err) {
-
-                    console.log(err);
-
-                    Lampa.Noty.show('KBTeam error');
-
-                });
-
-            });
-
-            setTimeout(function () {
-
-                var panel = $('.full-start-new__buttons');
-
-                if (panel.find('.kbteam-button').length === 0) {
-
-                    button.addClass('kbteam-button');
-
-                    panel.append(button);
-
+                if (!movie.kinopoisk_id) {
+                    console.log('KBTEAM: no kinopoisk_id');
+                    return;
                 }
 
-            }, 1000);
+                injectButton(movie);
+
+            }
+            catch (err) {
+
+                console.log('KBTEAM FULL ERROR:', err);
+
+            }
 
         });
 
