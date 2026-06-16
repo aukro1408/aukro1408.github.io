40d5d3e3bb40ad431ba086d175c5c3e447b6ac1d
aukro1408 <142673562+aukro1408@users.noreply.github.com>
2026-06-16 14:33:31 +0300

    Update vidbidge.js

--- vidbidge.js
+++ vidbidge.js
@@ -36,7 +36,52 @@
                 );
 
                 button.on('hover:enter', function() {
-                    window.open(url, '_blank');
+                    console.log('Trying internal browser...');
+                    if (Lampa.Activity && Lampa.Activity.push) {
+                        try {
+                            Lampa.Activity.push({
+                                url: url,
+                                title: 'VidBinge',
+                                component: 'browser'
+                            });
+                            console.log('VidBinge opened internally');
+                            return;
+                        } catch (e) {
+                            console.log('Browser component failed, trying iframe...');
+                        }
+
+                        try {
+                            Lampa.Activity.push({
+                                component: 'iframe',
+                                url: url,
+                                title: 'VidBinge'
+                            });
+                            console.log('VidBinge opened internally');
+                            return;
+                        } catch (e) {
+                            console.log('Iframe component failed, creating webview...');
+                        }
+                    }
+
+                    console.log('Trying iframe...');
+                    var container = activity.render();
+                    if (container && container.length) {
+                        var webview = $(
+                            '<div class="vidbinge-webview" style="position:fixed;top:0;left:0;width:100%;height:100%;z-index:9999;background:#000;">' +
+                                '<div class="vidbinge-webview-header" style="padding:10px;text-align:right;">' +
+                                    '<div class="selector" style="display:inline-block;padding:8px 16px;background:#333;color:#fff;border-radius:4px;">Close</div>' +
+                                '</div>' +
+                                '<iframe src="' + url + '" style="width:100%;height:calc(100% - 50px);border:none;"></iframe>' +
+                            '</div>'
+                        );
+
+                        webview.find('.selector').on('hover:enter', function() {
+                            webview.remove();
+                        });
+
+                        container.append(webview);
+                        console.log('VidBinge opened internally');
+                    }
                 });
 
                 torrentView.after(button);
