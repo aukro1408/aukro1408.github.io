83a0bf2de85e096bb8ad5e1a9af70ec757f314e7
aukro1408 <142673562+aukro1408@users.noreply.github.com>
2026-06-16 14:28:39 +0300

    Update vidbidge.js

--- vidbidge.js
+++ vidbidge.js
@@ -1,130 +1,51 @@
-/* Lampa Plugin: Watch Online (VidBinge) */
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  var pluginName = 'online';
+    console.log('VidBinge plugin loaded');
 
-  console.log('VidBinge plugin loaded');
+    Lampa.Listener.follow('full', function(e) {
+        if (e.type === 'complite') {
+            try {
+                var activity = e.object.activity;
+                if (!activity || !activity.render) return;
 
-  function getTmdbId(movie) {
-    if (!movie) return null;
-    var id = movie.tmdb_id || movie.id;
-    if (movie.movie_type && movie.movie_type === 'serial') {
-      return id;
-    }
-    if (movie.type && movie.type === 'serial') {
-      return id;
-    }
-    return id;
-  }
+                var torrentView = activity.render().find('.view--torrent');
+                if (!torrentView || !torrentView.length) return;
 
-  function isSerial(movie) {
-    if (!movie) return false;
-    return (
-      movie.movie_type === 'serial' ||
-      movie.type === 'serial' ||
-      (movie.is_serial !== undefined && movie.is_serial) ||
-      (movie.universal_type && movie.universal_type === 'serial')
-    );
-  }
+                if (torrentView.next('.vidbinge-button').length) return;
 
-  function buildUrl(movie) {
-    var tmdbId = getTmdbId(movie);
-    if (!tmdbId) return null;
-    if (isSerial(movie)) {
-      return 'https://vidbinge.to/tv/' + tmdbId;
-    }
-    return 'https://vidbinge.to/movie/' + tmdbId;
-  }
+                var movie = e.data.movie;
+                if (!movie) return;
 
-  function openUrl(url) {
-    if (!url) return;
-    if (typeof Lampa !== 'undefined' && Lampa.Activity && typeof Lampa.Activity.push === 'function') {
-      Lampa.Activity.push({ url: url });
-    } else if (typeof window !== 'undefined' && window.open) {
-      window.open(url);
-    } else if (typeof location !== 'undefined') {
-      location.href = url;
-    }
-  }
+                var tmdbId = movie.tmdb_id || movie.id;
+                if (!tmdbId) return;
 
-  function getButtonContainer() {
-    var selectors = [
-      '.full-start__buttons',
-      '.full-start__button-group',
-      '.full-start__info',
-      '.full-start__actions',
-      '.full-start'
-    ];
-    for (var i = 0; i < selectors.length; i++) {
-      var el = document.querySelector(selectors[i]);
-      if (el) return el;
-    }
-    return null;
-  }
+                var isSerial = movie.movie_type === 'serial' || movie.type === 'serial' || movie.is_serial;
+                var url;
 
-  function buttonExists() {
-    return document.querySelector('#vidbinge-online-btn, [data-vidbinge="true"]') !== null;
-  }
+                if (isSerial) {
+                    url = 'https://vidbinge.to/tv/' + tmdbId;
+                } else {
+                    url = 'https://vidbinge.to/movie/' + tmdbId;
+                }
 
-  function injectButton() {
-    if (buttonExists()) return;
+                var button = $(
+                    '<div class="full-start__button selector vidbinge-button">' +
+                        '<span>Watch Online</span>' +
+                    '</div>'
+                );
 
-    var movie = Lampa.Movie ? Lampa.Movie.get() : null;
-    if (!movie) return;
+                button.on('hover:enter', function() {
+                    window.open(url, '_blank');
+                });
 
-    var url = buildUrl(movie);
-    if (!url) return;
+                torrentView.after(button);
 
-    var container = getButtonContainer();
-    if (!container) return;
-
-    var btn = document.createElement('div');
-    btn.className = 'full-start__button selector';
-    btn.id = 'vidbinge-online-btn';
-    btn.setAttribute('data-vidbinge', 'true');
-    btn.setAttribute('data-tmdb', getTmdbId(movie) || '');
-
-    btn.innerHTML =
-      '<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor" style="vertical-align:middle;margin-right:6px;">' +
-        '<path d="M8 5v14l11-7z"/>' +
-      '</svg>' +
-      '<span>Watch Online</span>';
-
-    btn.addEventListener('click', function () {
-      openUrl(url);
+                console.log('VidBinge button added');
+            } catch (err) {
+                console.error('VidBinge plugin error:', err);
+            }
+        }
     });
 
-    container.appendChild(btn);
-    console.log('VidBinge button added');
-  }
-
-  function initPlugin() {
-    if (typeof Lampa === 'undefined') {
-      setTimeout(initPlugin, 500);
-      return;
-    }
-
-    Lampa.Listener.follow('full', function (e) {
-      if (e.type === 'complite' || e.type === 'render') {
-        setTimeout(injectButton, 300);
-      }
-    });
-
-    if (Lampa.Controller) {
-      var origTv = Lampa.Controller.toggle;
-      if (origTv) {
-        Lampa.Controller.toggle = function () {
-          origTv.apply(Lampa.Controller, arguments);
-          setTimeout(injectButton, 500);
-        };
-      }
-    }
-  }
-
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initPlugin);
-  } else {
-    initPlugin();
-  }
 })();
