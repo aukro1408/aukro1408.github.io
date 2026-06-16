87ee2e91291785ccb98dbd70cf9e0b6b82da59c6
aukro1408 <142673562+aukro1408@users.noreply.github.com>
2026-06-16 15:01:57 +0300

    Update vidbidge.js

--- vidbidge.js
+++ vidbidge.js
@@ -21,71 +21,98 @@
                 if (!tmdbId) return;
 
                 var isSerial = movie.movie_type === 'serial' || movie.type === 'serial' || movie.is_serial;
-                var url;
-
-                if (isSerial) {
-                    url = 'https://vidbinge.to/tv/' + tmdbId;
-                } else {
-                    url = 'https://vidbinge.to/movie/' + tmdbId;
-                }
 
                 var button = $(
                     '<div class="full-start__button selector vidbinge-button">' +
-                        '<span>Watch Online</span>' +
+                        '<span>VidBinge</span>' +
                     '</div>'
                 );
 
                 button.on('hover:enter', function() {
-                    console.log('Trying internal browser...');
-                    if (Lampa.Activity && Lampa.Activity.push) {
-                        try {
-                            Lampa.Activity.push({
-                                url: url,
-                                title: 'VidBinge',
-                                component: 'browser'
-                            });
-                            console.log('VidBinge opened internally');
-                            return;
-                        } catch (e) {
-                            console.log('Browser component failed, trying iframe...');
-                        }
+                    console.log('VidBinge button pressed');
 
-                        try {
-                            Lampa.Activity.push({
-                                component: 'iframe',
-                                url: url,
-                                title: 'VidBinge'
-                            });
-                            console.log('VidBinge opened internally');
-                            return;
-                        } catch (e) {
-                            console.log('Iframe component failed, creating webview...');
-                        }
-                    }
+                    var url = isSerial
+                        ? 'https://vidbinge.to/tv/' + tmdbId
+                        : 'https://vidbinge.to/movie/' + tmdbId;
 
-                    console.log('Trying iframe...');
+                    // Show loading indicator
                     var container = activity.render();
-                    if (container && container.length) {
-                        var webview = $(
-                            '<div class="vidbinge-webview" style="position:fixed;top:0;left:0;width:100%;height:100%;z-index:9999;background:#000;">' +
-                                '<div class="vidbinge-webview-header" style="padding:10px;text-align:right;">' +
-                                    '<div class="selector" style="display:inline-block;padding:8px 16px;background:#333;color:#fff;border-radius:4px;">Close</div>' +
-                                '</div>' +
-                                '<iframe src="' + url + '" style="width:100%;height:calc(100% - 50px);border:none;"></iframe>' +
-                            '</div>'
-                        );
+                    var loading = $(
+                        '<div class="vidbinge-loading" style="position:fixed;top:0;left:0;width:100%;height:100%;z-index:9999;background:rgba(0,0,0,0.85);display:flex;align-items:center;justify-content:center;color:#fff;font-size:18px;">' +
+                            'Loading...' +
+                        '</div>'
+                    );
+                    container.append(loading);
 
-                        webview.find('.selector').on('hover:enter', function() {
-                            webview.remove();
+                    // Fetch vidbinge page to extract vidora iframe URL
+                    fetch(url)
+                        .then(function(response) {
+                            if (!response.ok) throw new Error('HTTP ' + response.status);
+                            return response.text();
+                        })
+                        .then(function(html) {
+                            loading.remove();
+
+                            // Extract vidora iframe URL using regex
+                            var match = html.match(/<iframe[^>]*src="(https?:\/\/vidora\.stream\/embed\/[^"]+)"/i);
+                            if (match && match[1]) {
+                                var vidoraUrl = match[1];
+                                console.log('Found vidora URL:', vidoraUrl);
+
+                                // Open vidora URL in Lampa internal browser
+                                if (Lampa.Activity && Lampa.Activity.push) {
+                                    try {
+                                        Lampa.Activity.push({
+                                            url: vidoraUrl,
+                                            title: 'VidBinge',
+                                            component: 'browser'
+                                        });
+                                        return;
+                                    } catch (e) {
+                                        console.log('Browser component failed, trying iframe...');
+                                    }
+
+                                    try {
+                                        Lampa.Activity.push({
+                                            component: 'iframe',
+                                            url: vidoraUrl,
+                                            title: 'VidBinge'
+                                        });
+                                        return;
+                                    } catch (e) {
+                                        console.log('Iframe component failed, creating webview...');
+                                    }
+                                }
+
+                                // Fallback: create an embedded iframe
+                                var webview = $(
+                                    '<div class="vidbinge-webview" style="position:fixed;top:0;left:0;width:100%;height:100%;z-index:9999;background:#000;">' +
+                                        '<div class="vidbinge-webview-header" style="padding:10px;text-align:right;">' +
+                                            '<div class="selector" style="display:inline-block;padding:8px 16px;background:#333;color:#fff;border-radius:4px;">Close</div>' +
+                                        '</div>' +
+                                        '<iframe src="' + vidoraUrl + '" style="width:100%;height:calc(100% - 50px);border:none;" allowfullscreen allow="autoplay; fullscreen; encrypted-media; picture-in-picture"></iframe>' +
+                                    '</div>'
+                                );
+
+                                webview.find('.selector').on('hover:enter', function() {
+                                    webview.remove();
+                                });
+
+                                activity.render().append(webview);
+                            } else {
+                                console.error('Could not find vidora iframe URL in response');
+                                loading.text('Error: Could not find video source');
+                                setTimeout(function() { loading.remove(); }, 3000);
+                            }
+                        })
+                        .catch(function(err) {
+                            console.error('Fetch error:', err);
+                            loading.text('Error: ' + err.message);
+                            setTimeout(function() { loading.remove(); }, 3000);
                         });
-
-                        container.append(webview);
-                        console.log('VidBinge opened internally');
-                    }
                 });
 
                 torrentView.after(button);
-
                 console.log('VidBinge button added');
             } catch (err) {
                 console.error('VidBinge plugin error:', err);
